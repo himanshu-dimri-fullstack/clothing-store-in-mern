588c4a05f356b7910e9df471245ce8a970a12113
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-04-12 17:07:02 +0530

    Backend ready for deployment

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,13 +33,13 @@ export const loginUser = async (req, res) => {
         if (!user || !await bcrypt.compare(password, user.password)) {
             return res.status(401).json({ message: "Invalid credentials" });
         }
-        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1m' })
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' })
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
-            sameSite: "Lax",
-            // maxAge: 24 * 60 * 60 * 1000
-            maxAge: 60 * 1000
+            secure: true,
+            sameSite: "None",
+            maxAge: 24 * 60 * 60 * 1000
         });
         res.status(200).json({ user: { id: user._id, name: user.name, email: user.email, role: user.role } })
     }
